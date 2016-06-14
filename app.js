c1a25e239cb56485e5075d878136ca26abe1795c
nasser85 <nasser.abouelazm@gmail.com>
2016-06-14 18:10:21 -0400

    next commit

--- app.js
+++ app.js
@@ -1,29 +1,48 @@
 var express = require( 'express' );
 var app = express();
+var swig = require('swig');
+var tweetBank = require('./tweetBank');
+var routes = require('./routes/');
+
+
+
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+swig.setDefaults({ cache: false });
+app.use(express.static('public'));
+app.use('/', routes);
+//app.use('/public', express.static('stylesheets'));
+
+//app.get('/', function(req, res) {
+//	res.sendfile('/index.html');
+//});
+
+// var locals = {
+//     title: 'An Example',
+//     people: [
+//         { name: 'Gandalf'},
+//         { name: 'Frodo' },
+//         { name: 'Hermione'}
+//     ]
+// };
 
 
 
 
-app.use(function(req, res, next) {
-	res.send(req.method + " " + req.originalUrl);
-	res.end;
-	next();
-})
 
-app.get('/news', function(req, res, next) {
-	res.send("Hello, this is news");
-	res.end;
-	next();
-})
+//app.use(function(req, res, next) {
+	//res.send(req.method + " " + req.originalUrl);
+	//var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+	//res.render( 'index', locals );
+	//res.end;
+	//next();
+//})
+
+//app.get('/tweets/', tweetBank.find());
 
 
-app.post(function(req, res, next) {
-	var user_id = req.query;
-	// var token = req.body.token;
-	// var geo = req.body.geo;
-	res.send(req.method + " " + req.originalUrl);
-	next();
-})
+//app.post('/tweets/' tweetBank.add());
 /*
 app.use(function(err, req, res, next) {
 	console.log("Doesn't exist");
@@ -31,8 +50,8 @@ app.use(function(err, req, res, next) {
 })
 */
 
-app.delete('/news', function(req, res, next) {
-	res.send("Hello, this is news");
-})
+// app.delete('/news', function(req, res, next) {
+// 	res.send("Hello, this is news");
+// })
 
 app.listen(3000);
